7026f2055604b8fee7d392f6c1d4a54d42f8e715
Divyakonapala <92133595+Divyakonapala@users.noreply.github.com>
2021-10-11 20:10:20 +0530

    Create ifelseif.c

---
+++ Ifelseifgrades.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main()
+{
+int m;
+Printf("enter marks:");
+scanf("%d",&m);
+if(85<=m&&m<=100)
+{
+printf("Grade A");
+}
+else if(70<=m&&m<=84>)
+{
+printf("Grade B");
+}
+else if(55<=m&&m<=69)
+{
+printf("Grade C");
+}
+else if(40<=m&&<=54)
+{
+printf("Grade D");
+}
+else
+{
+printf("Grade F");
+}
+return 0;
+}
